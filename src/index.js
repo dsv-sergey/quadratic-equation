f1a2040c06d60b8db5a6fb0abe0c54093efc6303
Derevyannik Sergey <dsv-sergey@mail.ru>
2018-09-12 22:55:48 +0300

    quadratic-eqation ok

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function solveEquation(equation) {
   // your implementation
+  var abc = equation.replace(/\s/g, '').match(/\-?\d+/g);
+  var a = abc[0];
+  var b = abc[2];
+  var c = abc[3];
+  var d = Math.pow(b, 2) - (4 * a * c);
+  var result;
+  if (d > 0) {
+    var x1 = Math.round((-b + Math.sqrt(d)) / (2 * a));
+    var x2 = Math.round((-b - Math.sqrt(d)) / (2 * a));
+    result = [x1, x2];
+    result.sort((a,b) => a - b);
+    return result;
+  } else if (d = 0) {
+    result = -b / 2 * a;
+    return result;
+  } else {
+    return null;
+  }
 }
